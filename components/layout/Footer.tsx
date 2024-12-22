5bcb14573e9e8d8cd0bdb344d416d55ced16a34c
thatboyreegan <reaganronald491@gmail.com>
2024-12-23 01:55:18 +0300

    add: svg images for the listing page

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,47 +1,62 @@
 import React from 'react';
+import { Quicksand } from "next/font/google";
+
+const quickSand = Quicksand({
+    variable: "--font-quicksand",
+    subsets: ["latin"],
+  });
 
 const Footer: React.FC = () => {
   return (
-    <footer className='h-[351px] bg-[#222222] text-white'>
+    <footer className={`${quickSand.variable}  bg-[#222222] text-[#CACACA] text-sm`}>
         <div className='bg-[#34967C] w-full h-[29px]'></div>
-        <div className='grid grid-cols-2 gap-4 w-11/12 align-middle border-b border-white py-3'>
-            <article className='space-y-2'>
+        <div className='grid grid-cols-2  gap-4 w-7/8 mx-10 mt-6 border-b border-[#CACACA] py-3'>
+            <article className='space-y-2 max-w-[540px]  mb-16'>
                 <figure>
                     <img src="/assets/footerLogo.svg" alt="Footer Logo" />
                 </figure>
-                <p>ALX is a platform where travelers can discover and book unique, comfortable, and affordable lodging options worldwide. From cozy city apartments and tranquil countryside retreats to exotic beachside villas, ALX connects you with the perfect place to stay for any trip.</p>
+                <p className='font-medium'>ALX is a platform where travelers can discover and book unique, comfortable, and affordable lodging options worldwide. From cozy city apartments and tranquil countryside retreats to exotic beachside villas, ALX connects you with the perfect place to stay for any trip.</p>
             </article>
             <article className='flex justify-between'>
-                <section>
-                    <h4>Explore</h4>
+                <section className='text-sm'>
+                    <h4 className='pb-2 text-xl font-semibold'>Explore</h4>
                     <ul>
-                        <li>Apartments in Dubai</li>
-                        <li>Hotels in New York</li>
-                        <li>Villa in Spain</li>
-                        <li>Mansion in Indonesia</li>
+                        <li className='pb-1'>Apartments in Dubai</li>
+                        <li className='pb-1'>Hotels in New York</li>
+                        <li className='pb-1'>Villa in Spain</li>
+                        <li className='pb-1'>Mansion in Indonesia</li>
                     </ul>
                 </section>
-                <section>
-                    <h4>Company</h4>
+                <section className='text-sm'>
+                    <h4 className='pb-2 text-xl font-semibold'>Company</h4>
                     <ul>
-                        <li>About us</li>
-                        <li>Blog</li>
-                        <li>Career</li>
-                        <li>Customers</li>
-                        <li>Brand</li>
+                        <li className='pb-1'>About us</li>
+                        <li className='pb-1'>Blog</li>
+                        <li className='pb-1'>Blog</li>
+                        <li className='pb-1'>Career</li>
+                        <li className='pb-1'>Customers</li>
+                        <li className='pb-1'>Brand</li>
                     </ul>
                 </section>
-                <section>
-                    <h4>Help</h4>
+                <section className='text-sm'>
+                    <h4 className='pb-2 text-xl font-semibold'>Help</h4>
                     <ul>
-                        <li>Support</li>
-                        <li>Cancel booking</li>
-                        <li>Refunds Process</li>
+                        <li className='pb-1'>Support</li>
+                        <li className='pb-1'>Cancel booking</li>
+                        <li className='pb-1'>Refunds Process</li>
                     </ul>
                 </section>
             </article>
         </div>
-        <div></div>
+        <div className='grid grid-cols-2 w-7/8 mx-10 py-2'>
+            <p>Some hotel requires you to cancel more than 24 hours before check-in. Details <span className='text-[#34967c]'>here</span></p>
+            <ul className='flex justify-between'>
+                <li>Terms of Service</li>
+                <li>Policy Service</li>
+                <li>Cookies Policy</li>
+                <li>Partners</li>
+            </ul>
+        </div>
       
     </footer>
   );
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -14,7 +14,29 @@ const Header: React.FC = () => {
         <figure className='p-4'>
             <img src="/assets/logo.svg" alt="" />
         </figure>
-        <div className='text-black justify-center'>Search Bar</div>
+        <div className='text-black justify-center border-red-500 border w-fit'>
+            <form action="" className='flex divide-x-2'>
+                <div className=' border-black pl-2'>
+                <label htmlFor="UserLocation">Location</label>
+                <input type="text" className="outline-none bg-transparent" name="location" id="UserLocation" placeholder='Search for destination'/>
+                </div >
+                <div className='border-r-2 border-black pl-2'>
+                <label htmlFor="CheckIn">Check in</label>
+                <input type="text" id='CheckIn' className="outline-none bg-transparent" name='Checkin' placeholder='Add date'/>
+                </div>
+                <div className='border-r-2 border-black pl-2'>
+                <label htmlFor="CheckOut"> Check out</label>
+                <input type="text" id='CheckOut'className="outline-none bg-transparent" placeholder='Add date'/>
+                </div>
+                <div className='border-r-2 border-black pl-2'>
+                    <label htmlFor="People">People</label>
+                <input type="text" id='People' className="outline-none bg-transparent" placeholder='Add Guest'/>
+                </div>
+                
+                <button type='submit' className='rounded-full bg-black p-2 text-white'>Search
+                </button>
+            </form>
+        </div>
         <ul className='flex space-x-10 p-1 items-center justify-end'>
             <li className='bg-[#34967C] rounded-full h-6 w-20 text-center'>Sign In</li>
             <li className='bg-[#EBEBEB]  h-6 w-20 text-center border border-black rounded-full'>Sign Up</li>
